1f7d8aeee942717397bce2635d22bef0f6c666d2
hannanya2025 <hannanya.ai@gmail.com>
2025-07-24 23:37:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
